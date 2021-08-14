20bec0e89435ef263144de6f4ecdd8dcfb40b242
Aung Ko Zin <73013105+Sniper445-cell@users.noreply.github.com>
2021-08-14 17:05:58 +0630

    Add files via upload

---
+++ customer/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(MenuItem)
+admin.site.register(Category)
+admin.site.register(OrderModel)
+
+# Register your models here.
